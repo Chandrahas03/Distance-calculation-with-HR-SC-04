55628ceaf0c6b2004262936e61d6923c5f2a3eb4
Chandrahas03 <chandrahasreddy03@gmail.com>
2019-05-04 14:09:41 +0530

    Add files via upload

---
+++ DISTANCE_CALCULATION.ino
@@ -0,0 +1,52 @@
+#define TRIGGER 5
+#define ECHO    4
+ 
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "speedy-nodemcu.firebaseio.com"
+#define FIREBASE_AUTH "A1Az2TkGnSyTjHxK7EPTEYZRNi4nPE3L1yCtnamA"
+#define WIFI_SSID "Aviavatar"
+#define WIFI_PASSWORD "qwertyui"
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting"); 
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+ 
+void loop() {
+ 
+  long duration, distance;
+  digitalWrite(TRIGGER, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration/2) / 29.1;
+  
+  Serial.print(distance);
+  Serial.println("Centimeter:");
+  Firebase.setInt("Distance",distance);
+  delay(1000);
+}
+
+
